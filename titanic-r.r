4ad8f6f9cfb50f768250d6d8457291a636e7d30c
Fatima Khan <116169942+famakhan@users.noreply.github.com>
2024-06-11 12:57:48 +0100

    loading the first pieces of data  and cleaning it

---
+++ titanic-r.r
@@ -0,0 +1,172 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "576c5b3e",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "execution": {
+     "iopub.execute_input": "2024-06-11T11:57:36.218781Z",
+     "iopub.status.busy": "2024-06-11T11:57:36.216469Z",
+     "iopub.status.idle": "2024-06-11T11:57:37.504643Z",
+     "shell.execute_reply": "2024-06-11T11:57:37.502666Z"
+    },
+    "papermill": {
+     "duration": 1.296396,
+     "end_time": "2024-06-11T11:57:37.507680",
+     "exception": false,
+     "start_time": "2024-06-11T11:57:36.211284",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "── \u001b[1mAttaching core tidyverse packages\u001b[22m ──────────────────────── tidyverse 2.0.0 ──\n",
+      "\u001b[32m✔\u001b[39m \u001b[34mdplyr    \u001b[39m 1.1.4     \u001b[32m✔\u001b[39m \u001b[34mreadr    \u001b[39m 2.1.4\n",
+      "\u001b[32m✔\u001b[39m \u001b[34mforcats  \u001b[39m 1.0.0     \u001b[32m✔\u001b[39m \u001b[34mstringr  \u001b[39m 1.5.1\n",
+      "\u001b[32m✔\u001b[39m \u001b[34mggplot2  \u001b[39m 3.4.4     \u001b[32m✔\u001b[39m \u001b[34mtibble   \u001b[39m 3.2.1\n",
+      "\u001b[32m✔\u001b[39m \u001b[34mlubridate\u001b[39m 1.9.3     \u001b[32m✔\u001b[39m \u001b[34mtidyr    \u001b[39m 1.3.0\n",
+      "\u001b[32m✔\u001b[39m \u001b[34mpurrr    \u001b[39m 1.0.2     \n",
+      "── \u001b[1mConflicts\u001b[22m ────────────────────────────────────────── tidyverse_conflicts() ──\n",
+      "\u001b[31m✖\u001b[39m \u001b[34mdplyr\u001b[39m::\u001b[32mfilter()\u001b[39m masks \u001b[34mstats\u001b[39m::filter()\n",
+      "\u001b[31m✖\u001b[39m \u001b[34mdplyr\u001b[39m::\u001b[32mlag()\u001b[39m    masks \u001b[34mstats\u001b[39m::lag()\n",
+      "\u001b[36mℹ\u001b[39m Use the conflicted package (\u001b[3m\u001b[34m<http://conflicted.r-lib.org/>\u001b[39m\u001b[23m) to force all conflicts to become errors\n"
+     ]
+    },
+    {
+     "data": {
+      "text/html": [],
+      "text/latex": [],
+      "text/markdown": [],
+      "text/plain": [
+       "character(0)"
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    "# This R environment comes with many helpful analytics packages installed\n",
+    "# It is defined by the kaggle/rstats Docker image: https://github.com/kaggle/docker-rstats\n",
+    "# For example, here's a helpful package to load\n",
+    "\n",
+    "library(tidyverse) # metapackage of all tidyverse packages\n",
+    "\n",
+    "# Input data files are available in the read-only \"../input/\" directory\n",
+    "# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory\n",
+    "\n",
+    "list.files(path = \"../input\")\n",
+    "\n",
+    "# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using \"Save & Run All\" \n",
+    "# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "b090a1f7",
+   "metadata": {
+    "execution": {
+     "iopub.execute_input": "2024-06-11T11:57:37.548713Z",
+     "iopub.status.busy": "2024-06-11T11:57:37.515377Z",
+     "iopub.status.idle": "2024-06-11T11:57:40.049641Z",
+     "shell.execute_reply": "2024-06-11T11:57:40.047713Z"
+    },
+    "papermill": {
+     "duration": 2.54206,
+     "end_time": "2024-06-11T11:57:40.052612",
+     "exception": false,
+     "start_time": "2024-06-11T11:57:37.510552",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "Loading required package: lattice\n",
+      "\n",
+      "\n",
+      "Attaching package: ‘caret’\n",
+      "\n",
+      "\n",
+      "The following object is masked from ‘package:purrr’:\n",
+      "\n",
+      "    lift\n",
+      "\n",
+      "\n",
+      "The following object is masked from ‘package:httr’:\n",
+      "\n",
+      "    progress\n",
+      "\n",
+      "\n"
+     ]
+    }
+   ],
+   "source": [
+    "library(titanic)    # loads titanic_train data frame\n",
+    "library(caret)\n",
+    "library(tidyverse)\n",
+    "library(rpart)\n",
+    "\n",
+    "# 3 significant digits\n",
+    "options(digits = 3)\n",
+    "\n",
+    "# clean the data - `titanic_train` is loaded with the titanic package\n",
+    "titanic_clean <- titanic_train %>%\n",
+    "    mutate(Survived = factor(Survived),\n",
+    "           Embarked = factor(Embarked),\n",
+    "           Age = ifelse(is.na(Age), median(Age, na.rm = TRUE), Age), # NA age to median age\n",
+    "           FamilySize = SibSp + Parch + 1) %>%    # count family members\n",
+    "    select(Survived,  Sex, Pclass, Age, Fare, SibSp, Parch, FamilySize, Embarked)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30618,
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 7.632777,
+   "end_time": "2024-06-11T11:57:40.178680",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-06-11T11:57:32.545903",
+   "version": "2.5.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
